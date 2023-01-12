cba73db7a8be6df2fe3a05e2e10e68974fddde76
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-12 10:56:06 +0100

    add ApiServices

---
+++ lib/api.services.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import "package:fetch_api_test/models/user.dart";
+import "package:fetch_api_test/constants.dart";
+
+class ApiServices {
+  Future<List<User>> fetchPersons() async {
+    var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List<User> users = userFromJson(response.body);
+      return users;
+    } else {
+      throw Exception("Failed to load resources");
+    }
+  }
+}
