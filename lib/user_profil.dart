434c30008a1c5f249a386ff7902804e679765e77
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-13 09:07:24 +0100

    add user_profile

---
+++ lib/user_profil.dart
@@ -0,0 +1,13 @@
+import 'package:fetch_api_test/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserProfil extends StatelessWidget {
+  const UserProfil({Key? key, required this.user});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
