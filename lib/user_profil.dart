b445a03fde838b02adf8ff6f6873e8d59aefbc02
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-13 11:52:48 +0100

    add userData widget

--- lib/user_profil.dart
+++ lib/user_profil.dart
@@ -19,8 +19,29 @@ class UserProfil extends StatelessWidget {
             padding: const EdgeInsets.all(3.0),
             decoration:
                 BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-           
-              
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blueAccent)),
+                    child: Column(
+                      children: [
+                        Text("User data"),
+                        Text('ID: ${user.id}'),
+                        Text('Username : ${user.username}'),
+                        Text('Name : ${user.name}'),
+                        Text('Email : ${user.email}'),
+                        Text('Phone : ${user.phone}'),
+                        Text('Website : ${user.website}'),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
