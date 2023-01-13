437f4de170da6b766bb55df2e12e5eee3e198095
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-13 09:00:00 +0100

    add UserItem widget

---
+++ lib/widgets/user_item.dart
@@ -0,0 +1,42 @@
+import 'package:fetch_api_test/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserItem extends StatelessWidget {
+  const UserItem({Key? key, required this.user});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(2),
+        height: 140,
+        child: Card(
+          elevation: 5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(user.name),
+              Text(user.username),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () => {},
+                child: const Text("see more"),
+              )
+              //   onPressed: (() => Navigator.push(
+              //         context,
+              //         MaterialPageRoute(
+              //             builder: (context) => const SecondRoute()),
+              //       )),
+              //   child: const Text("See more"),
+              // )
+            ],
+          ),
+        ));
+  }
+}
--- lib/widgets/user_list.dart
+++ lib/widgets/user_list.dart
@@ -1,3 +1,4 @@
+import 'package:fetch_api_test/widgets/user_item.dart';
 import 'package:flutter/material.dart';
 import "package:fetch_api_test/models/user.dart";
 
@@ -11,36 +12,7 @@ class UserList extends StatelessWidget {
     return ListView.builder(
         itemCount: users!.length,
         itemBuilder: (context, index) {
-          return Container(
-              padding: const EdgeInsets.all(2),
-              height: 140,
-              child: Card(
-                elevation: 5,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(users![index].name),
-                    Text(users![index].username),
-                    TextButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
-                      ),
-                      onPressed: () => {},
-                      child: const Text("see more"),
-                    )
-                    //   onPressed: (() => Navigator.push(
-                    //         context,
-                    //         MaterialPageRoute(
-                    //             builder: (context) => const SecondRoute()),
-                    //       )),
-                    //   child: const Text("See more"),
-                    // )
-                  ],
-                ),
-              ));
+          return UserItem(user: users![index]);
         });
   }
 }
