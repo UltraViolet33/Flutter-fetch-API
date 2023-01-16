477ed58ab6cd01744b9b8cfdb24a017d6e22bc0e
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-16 08:20:05 +0100

    display user data in UserData Widget

--- lib/user_profil.dart
+++ lib/user_profil.dart
@@ -1,5 +1,6 @@
 import 'package:fetch_api_test/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:fetch_api_test/widgets/user_data.dart';
 
 class UserProfil extends StatelessWidget {
   const UserProfil({Key? key, required this.user});
@@ -22,23 +23,8 @@ class UserProfil extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.all(15.0),
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent)),
-                    child: Column(
-                      children: [
-                        Text("User data"),
-                        Text('ID: ${user.id}'),
-                        Text('Username : ${user.username}'),
-                        Text('Name : ${user.name}'),
-                        Text('Email : ${user.email}'),
-                        Text('Phone : ${user.phone}'),
-                        Text('Website : ${user.website}'),
-                      ],
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    ),
+                  UserData(
+                    user: user,
                   )
                 ],
               ),
--- lib/widgets/user_data.dart
+++ lib/widgets/user_data.dart
@@ -1,10 +1,37 @@
+import 'package:fetch_api_test/models/user.dart';
 import 'package:flutter/material.dart';
 
 class UserData extends StatelessWidget {
-  const UserData({Key? key}) : super(key: key);
+  const UserData({Key? key, required this.user});
+  final User user;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+      child: Column(
+        children: [
+          Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Text("User data"),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('ID: ${user.id}'),
+              Text('Username : ${user.username}'),
+              Text('Name : ${user.name}'),
+              Text('Email : ${user.email}'),
+              Text('Phone : ${user.phone}'),
+              Text('Website : ${user.website}'),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
